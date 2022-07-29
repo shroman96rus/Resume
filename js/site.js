a11851b50ea356253175ece1b554d7ef9954055e
shroman96rus <72510863+shroman96rus@users.noreply.github.com>
2022-07-29 17:23:09 +0500

    continue

--- js/site.js
+++ js/site.js
@@ -1,5 +1,10 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// let slideIndex = 1;
+// showSlides(slideIndex);
+
+document.querySelectorAll(".portfolio-item__slideshow-container").forEach(element => {
+  element.querySelectorAll(".slideshow-container__mySlides")[0].classList.remove("slide-high");
+  console.log(element.querySelectorAll(".slideshow-container__mySlides"))
+});
 
 // Next/previous controls
 function plusSlides(n) {
@@ -26,3 +31,10 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+document.querySelectorAll(".slideshow-container__prev").forEach(element => {
+  element.addEventListener("click", function(e){
+    console.log(e.currentTarget.parentNode);
+  });
+});

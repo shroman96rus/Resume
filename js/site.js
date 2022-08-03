1d4c988ceb7f11a5c72d88f22bcafddfadb094f4
shroman96rus <72510863+shroman96rus@users.noreply.github.com>
2022-08-03 17:28:39 +0500

    modal window done
    03.08.2022

--- js/site.js
+++ js/site.js
@@ -8,36 +8,10 @@ document.querySelectorAll(".portfolio-item__dots").forEach(element => {
     element.querySelectorAll(".dots__dot")[0].classList.add("active");
   });
 
-// Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("slideshow-container__mySlides");
-//   let dots = document.getElementsByClassName("dots__dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
 
 document.querySelector('.portfolio__content-flex').addEventListener('click',function(e)
 {
-    console.log(e.target);
+    console.log(e);
     if (e.target.className.includes('slideshow-container__prev')) {
         let sliders = e.target.parentNode.querySelectorAll(".slideshow-container__mySlides");
         let dots = e.target.parentNode.querySelectorAll(".dots__dot");
@@ -101,75 +75,29 @@ document.querySelector('.portfolio__content-flex').addEventListener('click',func
             }
         }
     }
+
+    if (e.target.localName == "img") {
+        document.querySelector("body").classList.add("body-block");
+        document.querySelector(".image-modal").classList.add("image-modal--active");
+        
+        let gettingImages = e.target.cloneNode(true);
+        console.log(gettingImages);
+        let divImageModal = document.createElement("div");
+        divImageModal.className = "image-modal__slideshow";
+        divImageModal.append(gettingImages);
+        document.querySelector(".image-modal__content").append(divImageModal);
+
+    }
+    
+    
 });
 
-//swiching left function
-// document.querySelectorAll(".slideshow-container__prev").forEach(element => {
-//   element.addEventListener("click", function(e){
-//     let sliders = e.currentTarget.parentNode.querySelectorAll(".slideshow-container__mySlides");
-//     //console.log(e.currentTarget.parentNode);
-//     for (let i = 0; i < sliders.length; i++) {
-//         if (sliders[i].className.includes("slide-visible")) {
-//             sliders[i].classList.remove("slide-visible");
-           
-//             if (i < 1) {
-//                 sliders[sliders.length-1].classList.add("slide-visible");
-//                 return;
-//             }
-//             if (i > 0 && i < sliders.length) {
-//                 sliders[i -1].classList.add("slide-visible");
-//                 return;
-//             }
-//         } 
-//     }
-   
-//   });
-// });
+document.querySelector('.image-modal__wrap').addEventListener('click',function(e){
+    
+    if (e.target.className == "image-modal__wrap" || e.target.className == "image-modal__close") {
+        document.querySelector("body").classList.remove("body-block");
+        document.querySelector(".image-modal").classList.remove("image-modal--active");
+        document.querySelector(".image-modal__content div").remove();
+    }
+});
 
-
-// //swiching right function
-// document.querySelectorAll(".slideshow-container__next").forEach(element => {
-//     element.addEventListener("click", function(e){
-//       let sliders = e.currentTarget.parentNode.querySelectorAll(".slideshow-container__mySlides");
-//       //console.log(e.currentTarget.parentNode);
-//       for (let i = 0; i < sliders.length; i++) {
-//           if (sliders[i].className.includes("slide-visible")) {
-//               sliders[i].classList.remove("slide-visible");
-             
-//               if (i >= sliders.length-1) {
-//                   sliders[0].classList.add("slide-visible");
-//                   return;
-//               }
-//               else{sliders[i+1].classList.add("slide-visible");}
-//               return;
-//             } 
-//         }
-     
-//     });
-//   });
-
-//   document.querySelectorAll(".dots__dot").forEach(element => {
-//     element.addEventListener("click", function(e){
-//       let sliders = e.currentTarget.parentNode.parentNode.querySelectorAll(".slideshow-container__mySlides");
-      
-//       const choiseSlider = e.currentTarget.getAttribute("number");
-//       const choiseDots = e.currentTarget.parentNode.querySelectorAll('.dots__dot');
-      
-//       for (let i = 0; i < sliders.length; i++) {
-//           if (sliders[i].className.includes("slide-visible")) {
-//               sliders[i].classList.remove("slide-visible");
-//             }
-//       }
-//       choiseDots.forEach(element => 
-//         {
-//             if (element.className.includes('active')) {
-//                 element.classList.remove('active');
-                
-//             }
-//         });
-
-//       sliders[choiseSlider-1].classList.add("slide-visible");
-//       e.currentTarget.classList.add('active');
-//       return;
-//     });
-//   });
